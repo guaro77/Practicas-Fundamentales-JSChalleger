a1c19ec1089b86ad9e3357fb12a3a19dd7eff4b9
angel <aperoza77@gmail.com>
2023-09-07 23:07:16 -0300

    practica-8

---
+++ fundamentals/practica-8..js
@@ -0,0 +1,11 @@
+
+// Escribe una función que tome una cadena como argumento. La cadena contiene la subcadena 'es'. Devuelve el índice de 'is
+
+const myFunction = (a) => {
+
+return a.indexOf('is')
+}
+
+console.log(myFunction("praise"))
+console.log(myFunction("risky"))
+console.log(myFunction("paris"))
